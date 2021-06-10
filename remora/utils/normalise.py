74bec3a5a42a361bb23075305874de2d6ca7c48d
Rasheed El-Bouri <rel-bouri@ONTOXFLINGPU01.oxfordnanolabs.local>
2021-06-10 13:58:32 +0100

    updating normalisation script

--- remora/utils/normalise.py
+++ remora/utils/normalise.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 
 
-class normaliser():
+class minmaxNormaliser():
+
+    #TODO: Add the normalisation methods that are generally used when analysing DNA/RNA reads
 
     def __init__(self, type):
 
@@ -10,8 +12,27 @@ class normaliser():
 
     def fit(self, x):
         if self.type == 'minmax':
-            self.maximum = x.to_numpy().max()
-            self.minimum = x.to_numpy().min()
+            if isinstance(x, pd.DataFrame):
+                self.maximum = x.to_numpy().max()
+                self.minimum = x.to_numpy().min()
+
+            elif isinstance(x, np.ndarray):
+                self.maximum = x.max()
+                self.minimum = x.min()
+
+            elif isinstance(x, list):
+                globMaxim = 0
+                globMinim = 0
+                for array in x:
+                    maxim = max(array)
+                    minim = min(array)
+                    if maxim > globMaxim:
+                        globMaxim = maxim
+                    if minim < globMinim:
+                        globMinim = minim
+
+                self.maximum = globMaxim
+                self.minimum = globMinim
 
 
     @property
@@ -23,9 +44,12 @@ class normaliser():
         return self.minimum
 
     def transform(self, x):
-        if isinstance(x, pd.DataFrame):
+        if isinstance(x, (pd.DataFrame, np.ndarray)):
             #return (x - x.to_numpy().min())/(x.to_numpy().max() - x.to_numpy().min())
             return (x - self._min)/(self._max - self._min)
 
-        else:
-            return (x - min(x))/(max(x) - min(x))
+
+        elif isinstance(x, list):
+            for array in x:
+                array = (array - self._min)/(self._max - self._min)
+            return x
