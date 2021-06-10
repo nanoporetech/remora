8b55773149d2c79be5a69bf454be0e3a6658bd43
Rasheed El-Bouri <rel-bouri@ONTOXFLINGPU01.oxfordnanolabs.local>
2021-06-10 15:56:00 +0100

    adding asymmetric base selection

--- remora/chunk_selection.py
+++ remora/chunk_selection.py
@@ -1,5 +1,5 @@
 
-def sample_chunks(read_data, number_to_sample, num_surrounding_bases,MOD_OFFSET,
+def sample_chunks(read_data, number_to_sample, bases_below, bases_above,MOD_OFFSET,
                     standardise=True, select_strands_randomly=True,
                     first_strand_index=0):
 
@@ -7,9 +7,8 @@ def sample_chunks(read_data, number_to_sample, num_surrounding_bases,MOD_OFFSET,
     Args:
         read_data: list of signal_mapping.SignalMapping objects
         number_to_sample: target number of data elements to return
-        num_surrounding_bases: include signal from bases around the central base;
-            this is symmetrical so a value of n will select n bases before and
-            after the potentially modified base
+        bases_below: number of bases to include from before the central base
+        bases_above: number of bases to include from after the central base
         standardise: returns standardised signals, otherwise unscaled
         select_strands_randomly: chooses a random read at each iteration
         first_strand_index: When select_strands_randomly==False, start reading
@@ -22,15 +21,21 @@ def sample_chunks(read_data, number_to_sample, num_surrounding_bases,MOD_OFFSET,
     if number_to_sample is None or number_to_sample == 0 or number_to_sample > n_reads:
         number_to_sample = n_reads
 
-    if num_surrounding_bases is None:
-        num_surrounding_bases = 0
+    if bases_below is None:
+        bases_below = 0
+
+    if bases_above is None:
+        bases_above = 0
 
 
     if not isinstance(number_to_sample, int):
         raise ValueError('number_to_sample must be an integer')
 
-    if not isinstance(num_surrounding_bases, int):
-        raise ValueError('num_surrounding_bases must be an integer')
+    if not isinstance(bases_below, int):
+        raise ValueError('bases_below must be an integer')
+
+    if not isinstance(bases_above, int):
+        raise ValueError('bases_above must be an integer')
 
     if not isinstance(first_strand_index, int):
         raise ValueError('first_strand_index must be an integer')
@@ -46,8 +51,8 @@ def sample_chunks(read_data, number_to_sample, num_surrounding_bases,MOD_OFFSET,
         ref = ''.join(alphabet_info.collapse_alphabet[b] for b in read.Reference)
         base_locs = read.Ref_to_signal - read.Ref_to_signal[0]
 
-        signalPoint1 = MOD_OFFSET - num_surrounding_bases
-        signalPoint2 = MOD_OFFSET + num_surrounding_bases
+        signalPoint1 = MOD_OFFSET - bases_below
+        signalPoint2 = MOD_OFFSET + bases_above
         if signalPoint1 <= 0:
             signalPoint1 = 0
             print('cannot select from negative bases, starting from first base')
