6b2f60d7e08f1bcb063b334bb9f73b7736609436
Rasheed El-Bouri <rel-bouri@ONTOXFLINGPU01.oxfordnanolabs.local>
2021-06-18 14:04:27 +0100

    updating with changes requested from chunk_selector MR

--- remora/models/mlp.py
+++ remora/models/mlp.py
@@ -4,18 +4,16 @@ import torch.nn.functional as F
 
 
 class MLP(nn.Module):
-
     def __init__(self, input_shape, dropout_rate=0.3):
         super().__init__()
 
         if dropout_rate > 1 or dropout_rate < 0:
-            raise ValueError('dropout must be between 0 and 1')
+            raise ValueError("dropout must be between 0 and 1")
 
         self.dropout_rate = dropout_rate
 
-        if not isinstance(input_shape,int):
-            raise ValueError('input_shape must be an integer shape')
-
+        if not isinstance(input_shape, int):
+            raise ValueError("input_shape must be an integer shape")
 
         self.fc1 = nn.Linear(input_shape, 50)
         self.fc2 = nn.Linear(50, 1)
